ff64e2bbaa0e0a28002888ef935c8d2b666b12c5
RINKU-CHEDRICK <rinkukalita17@gmail.com>
2025-05-18 19:19:42 +0530

    Update app.py
    movie title changed

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ movies = pd.DataFrame(movies_dict) #this is the dataframe
 #similartiy
 similarity = pickle.load(open('similarity.pkl','rb'))
 
-st.title('Movie Recommender System In ML ')
+st.title('Movie Recommender System  ')
 
 #select box
 #from streamlit API reference
