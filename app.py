5400ae4cca09478eda45a412aaf28d40af4b3f5a
RINKU-CHEDRICK <rinkukalita17@gmail.com>
2025-05-21 15:28:31 +0530

    BR

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ movies = pd.DataFrame(movies_dict) #this is the dataframe
 #similartiy
 similarity = pickle.load(open('similarity.pkl','rb'))
 
-st.title('RAHUL BORO')
+st.title('BORO RAHUL')
 
 #select box
 #from streamlit API reference
