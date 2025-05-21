4097cfdd4ac076f133fd17d4cc572f35602d7769
RINKU-CHEDRICK <rinkukalita17@gmail.com>
2025-05-21 18:35:24 +0530

    RC

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ movies = pd.DataFrame(movies_dict) #this is the dataframe
 #similartiy
 similarity = pickle.load(open('similarity.pkl','rb'))
 
-st.title('RECOMMENDER SYSTEM ')
+st.title('RECOMMENDER SYSTEM')
 
 #select box
 #from streamlit API reference
